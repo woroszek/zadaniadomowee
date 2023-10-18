6bf3661d6021b2cdffeea7d7a61605505e3fffc6
woroszek <woroszek@gmail.com>
2023-10-18 14:56:24 +0200

    dodano

---
+++ ProgramKsiegv2.py
@@ -0,0 +1,228 @@
+history = []
+warehouse = []
+
+def is_or_not(item):
+    if len(warehouse) > 0:
+        for i in warehouse:
+            if i[0] == item:
+                return True
+            
+        return False
+    else:
+        return False    
+    
+def all_items():
+    for i in warehouse:
+        ilosc = i[1]['ilosc']
+        koszt = i[1]['koszt']
+        print(f'{i[0]}({ilosc}x) w cenie {koszt} za sztukę. Łącznie: {ilosc*koszt}')    
+
+def item_in(item):
+    for i in warehouse:
+        if i[0] == item:
+            ilosc = i[1]['ilosc']
+            return ilosc
+        
+def item_cost(item):
+    for i in warehouse:
+        if i[0] == item:
+            koszt = i[1]['koszt']
+            return koszt
+
+def history_number(od, do, a=0):
+    od = int(od)
+    do = int(do)
+    if od > 0:
+        for i in history:
+            a = a + 1
+            if a < od or a > do:
+                continue
+            print(f'Opercja numer: {a} to  {i}')
+            print('***************************************************')    
+
+def warehouse_actual():
+    try:
+        if len(warehouse) < 1:
+            with open('warehouse.txt') as warehous:
+                for line in warehous:
+                    warehouse.append(eval(line))          
+    except:
+        pass
+
+def wareh_write():
+    with open("warehouse.txt", "w") as file:
+        for item in warehouse:
+            file.write(str(item) + "\n")
+
+def balance_his():
+    try:
+        with open('balance.txt') as balan:
+            xy = float(balan.read())                
+    except:
+        xy = 0
+    return xy
+
+def history_file():
+    try:
+        if len(history) == 0:
+            with open('history.txt') as file:
+                    for line in file:
+                        x = line.strip()
+                        history.append(x)    
+    except:
+        pass
+
+
+while True:
+    history_file()
+    warehouse_actual()
+    balance = balance_his()
+    print('Podaj komendę. Wpisz pomoc, jeśli chcesz zobaczyć dostępne komendy')
+    command = input().lower()
+    if command == 'pomoc':
+        print('s - dodanie lub odjecie kwoty z konta')
+        print('z - zakup')
+        print('sp - sprzedaż')
+        print('sk - stan konta')
+        print('st - stan magaznu')
+        print('m - stan magazynu dla konkretnego produktu')
+        print('h - historia działań aplikacji')
+        print('k - zamknięcie programu')
+
+    if command == 's':
+        print('Wpisz: D, jeśli chcesz dodać lub O, jeśli odjać: ')
+        command_s = input().lower()
+        if command_s == 'd':
+            add = input('Podaj kwotę do dodania: ')
+            if add.isnumeric() == True:
+                add = float(add)
+                balance += add
+                history.append((f'Dodano {add}'))
+            else:
+                print('Podano nieprawidłowe dane. Podaj liczbę.')
+        elif command_s == 'o':
+            sub = input('Podaj kwotę do odjęcia: ')
+            if sub.isnumeric() == True:
+                sub = float(sub)
+                if balance - sub < 0:
+                    print('Nieprawidlowe dane. Saldo Twojego konta nie może być ujemne.')
+                else:
+                    balance -= sub
+                    history.append(f'Odjeto {sub}')
+            else:
+                print('Podano nieprawidłowe dane. Podaj liczbę.')
+        else:
+            print('Nieprawidłowa komenda.')
+                
+    if command == 'z':
+        item = input('Podaj nazwę produktu: ').upper()
+        quantity = int(input('Podaj ilość zakupionego produktu: '))
+        cost = float(input('Podaj cenę produktu: '))
+        if is_or_not(item) == True:
+            for i in warehouse:
+                if balance - quantity * cost < 0:
+                        print('Zakup nie jest możliwy. Brak środków.') 
+                        break 
+                if i[0] == item and balance - quantity * cost >= 0:
+                    if i[1]['koszt'] != cost:
+                        print('Niestety. Podaleś różne ceny dla tego samego produktu.')
+                        break
+                        
+                    print('Produkt już znajduje się w magazynie. Ilość poprawnie zmieniona.')
+                    i[1]['ilosc'] += quantity
+                    history.append(f'Zaaktualizowano ilosc produktu {item} o {quantity}')
+                    balance = balance - quantity * cost
+                    wareh_write()
+                    break
+
+        if is_or_not(item) == False:
+            if balance - quantity * cost >= 0:
+                warehouse.append([item,{'ilosc': quantity,'koszt': cost}])
+                history.append(f'Dodano produkt {item} w cenie {cost} w ilości {quantity}')
+                balance = balance - quantity * cost
+                wareh_write()
+            else:
+                print('Brak środków na pokrycie zakupu.')
+        
+    if command == 'sp':
+        item = input('Podaj nazwę produktu: ').upper()
+        if is_or_not(item) == True:
+            print('Produkt znajduje się w magazynie.')
+            print(f'Mamy {item_in(item)} sztuk. Ile pragniesz sprzedać?')
+            quantity = int(input())
+            if quantity > item_in(item):
+                print(f'Podano liczbę większą niz stan magazynu. Nie możesz sprzedać więcej niż {item_in(item)}')
+            else:   
+                balance = balance + item_cost(item) * quantity
+                for a in warehouse:
+                    a[1]['ilosc'] -= quantity
+                    if item == a[0]:
+                        if a[1]['ilosc'] <= 0:
+                            history.append(f'Sprzedano {quantity} sztuk przedmiotu {item}')
+                            warehouse.remove(a)
+                            wareh_write()
+                            break
+
+                        else:
+                            history.append(f'Sprzedano {quantity} sztuk przedmiotu {item}')
+                            wareh_write()
+                            break    
+        else:
+            print(f'Brak produktu {item} na stanie magazynu.')                
+    
+    if command == 'sk':
+        try:
+            with open('balance.txt','r') as file:
+                print(f'Stan Twojego konta to: {file.read()}')
+        except:
+            print(f'Stan Twojego konta to: {balance}')
+
+    if command == 'st':
+        if len(warehouse) < 1:
+            print('Magazyn jest pusty.')
+        else:
+            print('Stan magazynu:')
+            for i in warehouse:
+                ilosc = i[1]['ilosc']
+                koszt = i[1]['koszt']
+                print(f'{i[0]}({ilosc}x) w cenie {koszt} za sztukę. Wartość łącznie: {ilosc*koszt}')
+
+    if command == 'm':
+        item = input('Podaj nazwę produktu: ').upper()
+        if is_or_not(item) == True:
+            for i in warehouse:
+                if i[0] == item:
+                    ilosc = i[1]['ilosc']
+                    cena = i[1]['koszt']
+                    print('Stan magazynu:')
+                    print(f'Dla produktu {item}, to {ilosc}. Cena jednej sztuki: {cena}.')
+
+    if command == 'h':
+        if len(history) > 0:
+            print(f'Wykonano łącznie {len(history)} operacji.')
+            print('Możesz wybrać zakres operacji. ')
+            print(f'Możliwy przedział to: 1 do {len(history)}')
+            od = input("Wprowadź numer operacji od której pragniesz zaczać.")
+            if od.isnumeric() == False or int(od) < 1 or int(od) > len(history):
+                print(f'Błędna wartość. Możliwy przedział to: 1 do {len(history)}')
+                print('Wyświetlam wyniki od operacji pierwszej.')
+                od = 1
+            do = input("Wprowadź numer operacji na której pragniesz zakończyć.")
+            if do.isnumeric() == False or int(do) > len(history) or int(od) > int(do):
+                print(f'Błędna wartość. Możliwy przedział to: 1 do {len(history)}')
+                print('Wyświetlam wyniki do samego końca.')
+                do = int(len(history))
+            history_number(od, do)
+        else:
+            print("Historia wykonanych operacji jest pusta.")
+
+    with open("balance.txt", "w") as text:
+        text.write(str(balance))
+    
+    if len(history) > 0:
+        with open("history.txt", "w") as file:
+            for item in history:
+                file.write(str(item) + '\n') 
+
+    if command == 'k':
+        break
