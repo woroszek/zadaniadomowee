1cb899ec9bb5dada765f1a5513a2056fc162a096
woroszek <woroszek@gmail.com>
2023-11-01 18:27:24 +0100

    dodano

---
+++ reader.py
@@ -0,0 +1,33 @@
+import sys, csv
+
+csv_items = []
+csv_in = sys.argv[1]
+csv_out = sys.argv[2]
+item_to_modi = []
+
+try:
+    with open(csv_in) as f:
+        reader = csv.reader(f)
+        for line in reader:
+            csv_items.append(line)
+
+    for x in sys.argv[3:]:
+        item_to_modi.append(x.split(','))
+
+    for y in item_to_modi:
+        csv_items[int(y[1])][int(y[0])] = y[2]
+
+    with open(csv_out, 'w', newline='') as f:
+        csvwriter = csv.writer(f, delimiter=',')
+        for item in csv_items:
+            csvwriter.writerow(item)
+    print('Twój plik po zmianaych wygląda następująco:')
+    print('------------------------------')
+    for i in csv_items:
+        print(' | '.join(str(x) for x in i))
+    print('------------------------------')
+except IndexError:
+    print('Poprawna instrukcja dla programu to:')
+    print('python reader.py <plik_wejsciowy> <plik_wyjsciowy> <zmiana_1> <zmiana_2> ... <zmiana_n>')
+except FileNotFoundError:
+    print('Podany plik, który pragniesz zmodyfikować, nie istnieje')
